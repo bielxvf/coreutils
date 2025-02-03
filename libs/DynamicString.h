21e001321e7abbd6dd961143dd4f910bc5fa6b31
Biel <bielxvf@gmail.com>
2025-02-03 14:55:25 +0100

    [DynamicString.h] Modified to compile

--- libs/DynamicString.h
+++ libs/DynamicString.h
@@ -125,13 +125,19 @@ void DS_insert_ch(DS* ds, uint64_t pos, char c)
 
 void DS_insert_cstr(DS* ds, uint64_t pos, char* cstr)
 {
+    (void) ds;
+    (void) pos;
+    (void) cstr;
 }
 
 void DS_insert_ds(DS* ds, uint64_t pos, const DS* dsubstr)
 {
+    (void) ds;
+    (void) pos;
+    (void) dsubstr;
 }
 
-char* DS_as_cstr(const DS* ds)
+const char* DS_as_cstr(const DS* ds)
 {
     return (const char*) ds->data;
 }
@@ -153,75 +159,88 @@ DS DS_substring(const DS* ds, uint64_t start, uint64_t length)
 DS DS_head(const DS* ds, uint64_t end)
 {
     DS dshead = DS_from_cstr("");
-    DS_push_back_n_cstr(&dshead, ds->data, end);
+    DS_push_back_n_cstr(&dshead, (const char*) ds->data, end);
     return dshead;
 }
 
 DS DS_tail(const DS* ds, uint64_t start)
 {
     DS dstail = DS_from_cstr("");
-    DS_push_back_cstr(&dstail, ds->data + start);
+    DS_push_back_cstr(&dstail, (const char*) ds->data + start);
     return dstail;
 }
 
-bool DS_compare(const DS* dsl, const char* cstr)
+bool DS_compare_cstr(const DS* dsl, const char* cstr)
 {
     return strcmp((const char*) dsl->data, cstr) == 0;
 }
 
-bool DS_compare(const DS* dsl, const DS* dsr)
+bool DS_compare_ds(const DS* dsl, const DS* dsr)
 {
-    return DS_compare(dsl, (const char*) dsr->data);
+    return DS_compare_cstr(dsl, (const char*) dsr->data);
 }
 
-bool DS_contains(const DS* ds, const char* csubstr)
+bool DS_contains_cstr(const DS* ds, const char* csubstr)
 {
     return strstr((const char*) ds->data, csubstr);
 }
 
-bool DS_contains(const DS* ds, const DS* dsubstr)
+bool DS_contains_ds(const DS* ds, const DS* dsubstr)
 {
-    return DS_contains(ds, (const char*) dsubstr->data);
+    return DS_contains_cstr(ds, (const char*) dsubstr->data);
 }
 
 void DS_trim_leading(DS* ds)
 {
+    (void) ds;
 }
 
 void DS_trim_trailing(DS* ds)
 {
+    (void) ds;
 }
 
 void DS_trim(DS* ds)
 {
+    (void) ds;
 }
 
 void DS_to_lower(DS* ds)
 {
+    (void) ds;
 }
 
 void DS_to_upper(DS* ds)
 {
+    (void) ds;
 }
 
 void DS_remove_ch(DS* ds, uint64_t pos)
 {
+    (void) ds;
+    (void) pos;
 }
 
-void DS_remove_substr(DS* ds, const char* csubstr)
+void DS_remove_cstr(DS* ds, const char* csubstr)
 {
+    (void) ds;
+    (void) csubstr;
 }
 
-void DS_remove_substr(DS* ds, const DS* dsubstr)
+void DS_remove_ds(DS* ds, const DS* dsubstr)
 {
-    DS_remove_substr(ds, (const char*) dsubstr->data);
+    DS_remove_cstr(ds, (const char*) dsubstr->data);
 }
 
-DS* DS_split(const DS* ds, char delim, uint64_t* count)
+DS* DS_split_ch(const DS* ds, char delim, uint64_t* count)
 {
+    (void) ds;
+    (void) delim;
+    (void) count;
+    return NULL;
 }
 
-uint64_t DS_find(const DS* ds, char c)
+uint64_t DS_find_ch(const DS* ds, char c)
 {
     for (uint64_t i = 0; i < ds->len; i++) {
         if (c == ds->data[i]) {
@@ -231,7 +250,7 @@ uint64_t DS_find(const DS* ds, char c)
     return UINT64_MAX;
 }
 
-uint64_t DS_find(const DS* ds, const char* cstr)
+uint64_t DS_find_cstr(const DS* ds, const char* cstr)
 {
     uint64_t cstr_len = strlen(cstr);
     if (cstr_len > ds->len) {
@@ -239,12 +258,17 @@ uint64_t DS_find(const DS* ds, const char* cstr)
     }
 
     for (uint64_t i = 0; i < ds->len; i++) {
-        if (strcmp(ds->data + i, cstr) == 0) {
+        if (strcmp((const char*) ds->data + i, cstr) == 0) {
             return i;
         }
     }
 
     return UINT64_MAX;
+}
+
+uint64_t DS_find_ds(const DS* dsl, const DS* dsr)
+{
+    return DS_find_cstr(dsl, (const char*) dsr->data);
 }
 
 uint64_t DS_find_last(const DS* ds, char c)
@@ -259,7 +283,7 @@ uint64_t DS_find_last(const DS* ds, char c)
     return UINT64_MAX;
 }
 
-uint64_t DS_find_last(const DS* ds, const char* cstr)
+uint64_t DS_find_last_cstr(const DS* ds, const char* cstr)
 {
     uint64_t cstr_len = strlen(cstr);
     if (cstr_len > ds->len) {
@@ -267,7 +291,7 @@ uint64_t DS_find_last(const DS* ds, const char* cstr)
     }
 
     for (uint64_t i = ds->len - 1;; i--) {
-        if (strcmp(ds->data + i, cstr) == 0) {
+        if (strcmp((const char*) ds->data + i, cstr) == 0) {
             return i;
         } else if (0 == i) {
             return UINT64_MAX;
@@ -275,6 +299,11 @@ uint64_t DS_find_last(const DS* ds, const char* cstr)
     }
 
     return UINT64_MAX;
+}
+
+uint64_t DS_find_last_ds(const DS* dsl, const DS* dsr)
+{
+    return DS_find_last_cstr(dsl, (const char*) dsr->data);
 }
 
 #endif // DS_IMPLEMENTATION

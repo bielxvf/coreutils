0408af1d3613eb8b621db8a393a5709d34a12c55
Biel <bielxvf@gmail.com>
2025-02-03 15:07:44 +0100

    [basename.c] Implement basic basename functionality. Needs fixing.

--- src/basename.c
+++ src/basename.c
@@ -8,12 +8,15 @@
 
 int main(int argc, char** argv)
 {
-    DS args[argc];
-    for (uint64_t i = 0; i < (uint64_t) argc; i++) {
-        args[i] = DS_from_cstr(argv[i]);
+    DS args[argc - 1];
+    for (uint64_t i = 1; i < (uint64_t) argc; i++) {
+        args[i - 1] = DS_from_cstr(argv[i]);
     }
-    for (uint64_t i = 0; i < (uint64_t) argc; i++) {
-        printf("%lu: %s\n", i, DS_as_cstr(&args[i]));
+
+    // TODO FIX: basename "/etc/portage/" should return "portage", not an empty string
+    for (uint64_t i = 0; i < (uint64_t) argc - 1; i++) {
+        DS tail = DS_tail(&args[i], DS_find_last_ch(&args[i], '/') + 1);
+        printf("%s\n", DS_as_cstr(&tail));
     }
     return 0;
 }

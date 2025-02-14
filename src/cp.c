1e1703e56e0a125f029fb2c501953c52cbc19c11
Biel <bielxvf@gmail.com>
2025-02-14 22:08:59 +0100

    [cp] Includes, stat

--- src/cp.c
+++ src/cp.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <sys/types.h>
 #include <stdint.h>
 
 #include "../libs/Args.h"
@@ -27,6 +30,18 @@ int main(int argc, char** argv)
             .type = ARG_NONE,
             .is_set = false,
         },
+        {
+            .l = DS_from_cstr("--recursive"),
+            .s = DS_from_cstr("-r"),
+            .type = ARG_NONE,
+            .is_set = false,
+        },
+        {
+            .l = DS_from_cstr("--interactive"),
+            .s = DS_from_cstr("-i"),
+            .type = ARG_NONE,
+            .is_set = false,
+        },
     };
 
     Args args;
@@ -44,23 +59,49 @@ int main(int argc, char** argv)
 
     switch(args.count) {
     case 0: {
-        fprintf(stderr, "Error: No arguments\n");
+        fprintf(stderr, PROGRAM": No arguments\n");
         print_help();
         return 1;
     } break;
     case 1: {
-        fprintf(stderr, "Error: Missing source and destination\n");
+        fprintf(stderr, PROGRAM": Missing source and destination\n");
         print_help();
         return 1;
     } break;
     case 2: {
-        fprintf(stderr, "Error: Missing destination for '%s'\n", DS_to_cstr(&args.data[1]));
+        fprintf(stderr, PROGRAM": Missing destination for '%s'\n", DS_to_cstr(&args.data[1]));
         print_help();
         return 1;
     } break;
     }
 
-    // Actual copying
+    struct stat path_stat;
+    stat(DS_to_cstr(&args.data[args.count - 1]), &path_stat);
+    if (S_ISDIR(path_stat.st_mode)) {
+        // @TODO
+        // copy everything from args.data[1] to args.data[args.count - 2] to
+        // directory args.data[args.count - 1]
+        return 0;
+    }
+
+    if (program_options[3].is_set) {
+        struct stat out_stat;
+        stat(DS_to_cstr(&args.data[args.count - 1], &out_stat);
+        if (S_ISREG(out_stat.st_mode)) {
+            // @TODO
+            // Check file exists
+            // Prompt the user accordingly
+            return 0;
+        } else if (S_ISDIR(out_stat.st_mode)) {
+            // @TODO
+            // Check file exists in the dir
+            // Prompt the user accordingly
+            return 0;
+        } else {
+            fptintf(stderr, PROGRAM": Destination appears not to be a regular file or directory\n");
+            return 1;
+        }
+    }
 
     FILE* input_file = fopen(DS_to_cstr(&args.data[1]), "rb");
     fseek(input_file, 0, SEEK_END);
@@ -71,7 +112,6 @@ int main(int argc, char** argv)
     fread(buffer, input_file_size, 1, input_file);
     fclose(input_file);
 
-    // @NOTE: This will replace the file, we need to handle options like -i
     FILE* output_file = fopen(DS_to_cstr(&args.data[2]), "wb");
     fwrite(buffer, input_file_size, 1, output_file); 
     fclose(output_file);

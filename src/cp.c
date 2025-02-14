0be9b8c434d85f38bcc9da7e2146ebb8428cabbd
Biel <bielxvf@gmail.com>
2025-02-14 20:59:14 +0100

    [cp] Actually copying the file

--- src/cp.c
+++ src/cp.c
@@ -42,5 +42,39 @@ int main(int argc, char** argv)
         return 0;
     }
 
+    switch(args.count) {
+    case 0: {
+        fprintf(stderr, "Error: No arguments\n");
+        print_help();
+        return 1;
+    } break;
+    case 1: {
+        fprintf(stderr, "Error: Missing source and destination\n");
+        print_help();
+        return 1;
+    } break;
+    case 2: {
+        fprintf(stderr, "Error: Missing destination for '%s'\n", DS_to_cstr(&args.data[1]));
+        print_help();
+        return 1;
+    } break;
+    }
+
+    // Actual copying
+
+    FILE* input_file = fopen(DS_to_cstr(&args.data[1]), "rb");
+    fseek(input_file, 0, SEEK_END);
+    size_t input_file_size = ftell(input_file);
+    rewind(input_file);
+
+    char* buffer = (char*) malloc(input_file_size + 1);
+    fread(buffer, input_file_size, 1, input_file);
+    fclose(input_file);
+
+    // @NOTE: This will replace the file, we need to handle options like -i
+    FILE* output_file = fopen(DS_to_cstr(&args.data[2]), "wb");
+    fwrite(buffer, input_file_size, 1, output_file); 
+    fclose(output_file);
+    
     return 0;
 }
